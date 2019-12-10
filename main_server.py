68994a777a39302ebe01e2c6c7a1a404db638a7d
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-12-10 10:16:19 +0300

    Update main_server.py

--- main_server.py
+++ main_server.py
@@ -1,7 +1,7 @@
 import json
-
 import trio
 from trio_websocket import serve_websocket, ConnectionClosed
+from helpers import open_chrome_browser
 
 # MSG = {
 #   "msgType": "Buses",
@@ -10,9 +10,9 @@ from trio_websocket import serve_websocket, ConnectionClosed
 #     {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
 #   ]
 # }
-
 #MSG = {"msgType": "Buses", "buses": [{"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"}]}
-MSG = {'msgType': 'Buses', 'buses': [{'busId': 'a134aa', 'lat': 55.747629944737, 'lng': 37.641726387317, 'route': '156'}]}
+#MSG = {'msgType': 'Buses', 'buses': [{'busId': 'a134aa', 'lat': 55.747629944737, 'lng': 37.641726387317, 'route': '156'}]}
+
 
 def get_json_route(path='156.json'):
     with open(path, 'r', encoding='utf-8') as fl:
@@ -28,7 +28,6 @@ def get_route():
              "lat": coordinate[0],
              "lng": coordinate[1],
              "route": name} for coordinate in coordinates]
-
     return [{"msgType": "Buses", "buses": [dot]} for dot in dots]
 
 
@@ -53,15 +52,7 @@ async def start():
 
 
 def main():
-    # import webbrowser
-    # import os
-    # webbrowser.register('Chrome',
-    #                     None,
-    #                     webbrowser.BackgroundBrowser(
-    #                         r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'))
-    #
-    # _path = os.path.join(os.getcwd(), 'index.html')
-    # webbrowser.get('Chrome').open(_path)
+    open_chrome_browser()
     trio.run(start)
 
 
