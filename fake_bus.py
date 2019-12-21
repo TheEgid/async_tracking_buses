43eceef0b56b2aff9e4b02fba685e723cb6784fc
TheEgid <egid.f1@gmail.com>
2019-12-22 00:23:53 +0300

    Add a load!

--- fake_bus.py
+++ fake_bus.py
@@ -47,14 +47,35 @@ async def run_bus(url, bus_id, route):
                    print('Connection attempt failed: %s' % ose, file=sys.stderr)
 
 
-async def start_buses():
+async def transponder(url, receive_channel):
     async with trio.open_nursery() as nursery:
-        for _buses_qty in range(10, random.randint(20, 25)):
-            for raw_route in load_routes():
-                bus_id = generate_bus_id(raw_route['name'], generate_unical_hash())
-                handler = partial(run_bus, url='ws://127.0.0.1:8080/ws',
-                                  bus_id=bus_id, route=raw_route)
-                nursery.start_soon(handler)
+        async for value in receive_channel:
+            raw_route = dict(value)
+            bus_id = generate_bus_id(raw_route['name'],
+                                     generate_unical_hash())
+            handler = partial(run_bus, url=url, bus_id=bus_id,
+                              route=raw_route)
+            nursery.start_soon(handler)
+            await trio.sleep(random.random())
+
+
+async def send_updates(send_channel):
+    for _buses_qty in range(2):  # 5, random.randint(7, 10)):
+        for raw_route in load_routes():
+            await send_channel.send(raw_route)
+
+
+async def start_buses():
+    channel_qty = 10
+    all_channels = list()
+    url = 'ws://127.0.0.1:8080/ws'
+    async with trio.open_nursery() as nursery:
+        for free_open_memory_channel in range(channel_qty):
+            all_channels.append([trio.open_memory_channel(0)])
+        for all_channel in all_channels:
+            send_channel, receive_channel = all_channel[0]
+            nursery.start_soon(send_updates, send_channel)
+            nursery.start_soon(transponder, url, receive_channel)
 
 
 def main():
