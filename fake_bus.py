98931ca2c513d4ff574c4ad17259089c86238a91
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-12-23 17:18:11 +0300

    Update fake_bus.py

--- fake_bus.py
+++ fake_bus.py
@@ -3,11 +3,11 @@ import os
 import random
 import sys
 from functools import partial
-
-import helpers
+from trio_websocket import open_websocket_url
 import trio
 from helpers import install_logs_parameters
-from trio_websocket import open_websocket_url
+import helpers
+import settings
 
 
 def get_serialized_bus_info(route, bus_id):
@@ -40,7 +40,7 @@ async def run_bus(url, route, bus_id):
                 async with open_websocket_url(url) as ws:
                     broadcast_logger.info(f'{serialized_bus_info}')
                     await ws.send_message(serialized_bus_info)
-                    await trio.sleep(helpers.PAUSE_DUR)
+                    await trio.sleep(args.refresh_timeout)
             except OSError as ose:
                 print('Connection attempt failed: %s' % ose, file=sys.stderr)
 
@@ -57,7 +57,7 @@ async def transponder(url, bus_id, receive_channel):
 async def send_updates(url, all_channels):
         async with trio.open_nursery() as nursery:
             for raw_route in load_routes():
-                for _buses_qty in range(2):
+                for bus_per_route in range(args.buses_per_route):
                     current_channel = random.choice(list(all_channels))
                     send_channel, receive_channel = all_channels[current_channel]
                     bus_id = _buses_qty
@@ -66,18 +66,34 @@ async def send_updates(url, all_channels):
 
 
 async def start_buses():
-    possible_channels_qty = 50
     all_channels = {}
-    url = 'ws://127.0.0.1:8080/ws'
-    for id, free_open_memory_channel in enumerate(range(possible_channels_qty)):
+    url = args.server
+    for id, free_open_memory_channel in enumerate(range(args.websockets_number)):
         all_channels[id] = trio.open_memory_channel(0)
     async with trio.open_nursery() as nursery:
         nursery.start_soon(send_updates, url, all_channels)
 
 
+def get_args_parser():
+    formatter_class = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=formatter_class)
+	
+    parser.add_argument('-server', default=settings.SERVER)
+    parser.add_argument('-routes_number', type=int, default=settings.ROUTES_NUMBER)
+    parser.add_argument('-buses_per_route', type=int, default=settings.BUSES_PER_ROUTE)
+    parser.add_argument('-websockets_number', type=int, default=settings.WEBSOCKETS_NUMBER)
+	parser.add_argument('-emulator_id', default=settings.EMULATOR_ID)
+    parser.add_argument('-refresh_timeout', type=int, default=settings.REFRESH_TIMEOUT)	
+    parser.add_argument('-v', action='store_true', default=settings.V, 
+						help='check logs')
+    return parser        
+
+
 def main():
     global broadcast_logger
     broadcast_logger = install_logs_parameters(True)
+    
+    args = get_args_parser().parse_args()
     trio.run(start_buses)
 
 
