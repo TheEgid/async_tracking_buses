a7f3dff79485122ede2125fff50e3c59c97ef359
TheEgid <egid.f1@gmail.com>
2020-01-03 14:52:01 +0300

    Add json schema

--- helpers.py
+++ helpers.py
@@ -1,14 +1,15 @@
+import json
 import logging
-import os
-import platform
-import webbrowser
 from dataclasses import dataclass
+
+import jsonschema
 
 BUSES = {}
 BUSES_COUNTER = set()
 
 conn_attempt = 0
 broadcast_logger = None
+server_logger = None
 _args = None
 
 # x1, y1, x2, y2, x, y = [int(input()) for i in range(6)]
@@ -25,19 +26,6 @@ class Bus:
     route: str
 
 
-def open_chrome_browser():
-    os_name = platform.system()
-    if os_name == 'Windows':
-        chrome_path = r'"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %s'
-    elif os_name == 'Linux':
-        chrome_path = r'"/usr/bin/google-chrome" %s'
-    else:
-        raise ValueError(f'unknown os: {os_name=}!')
-
-    webbrowser.register('Chrome', None, webbrowser.BackgroundBrowser(chrome_path))
-    link_path = os.path.join(os.getcwd(), 'index.html')
-    webbrowser.get(chrome_path).open(link_path)
-
 
 def install_logs_parameters(logs=False):
     level = logging.CRITICAL
@@ -48,3 +36,14 @@ def install_logs_parameters(logs=False):
     logging.basicConfig(format=str_format, datefmt=date_format, level=level)
     broadcast_logger = logging.getLogger()
     return broadcast_logger
+
+
+def validate_input_json_data(json_data):
+    with open('bounds_schema.json', 'r') as f:
+        schema_data = f.read()
+        json_schema = json.loads(schema_data)
+    try:
+        jsonschema.validate(json_data, json_schema)
+    except jsonschema.exceptions.ValidationError:
+        return None
+    return json_data
